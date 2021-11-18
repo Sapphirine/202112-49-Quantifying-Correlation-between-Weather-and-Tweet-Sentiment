1838d571d3015c037a873ff676a1d8d97f45f54f
Yewen Zhou <yewenzhou@berkeley.edu>
2021-11-17 22:03:25 -0500

    Add readtwittercsv

--- gettwitter.py
+++ gettwitter.py
@@ -14,7 +14,7 @@ auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
 auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-RUNTIME = 600
+RUNTIME = 10
 
 res = []
 
