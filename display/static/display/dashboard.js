00bfe4d9a5de424f7985d811ef8b0766fae0fefa
Yewen Zhou <yewenzhou@berkeley.edu>
2021-12-12 16:11:23 -0500

    Added countdown

--- display/static/display/dashboard.js
+++ display/static/display/dashboard.js
@@ -1,18 +1,36 @@
 /* globals Chart:false, feather:false */
+function startTimer(duration, display) {
+  let timer = duration, minutes, seconds;
+  setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = minutes + ":" + seconds;
+
+    if (--timer < 0) {
+      timer = duration;
+    }
+  }, 1000);
+}
+
 document.getElementById("twitter_update").addEventListener("click", function () {
   let progress = document.createElement("div");
   progress.className = "progress";
   progress.style.height = "20px";
   progress.innerHTML = `
     <div class="progress-bar" role="progressbar" 
-    style="width: 25%;" 
-    aria-valuenow="25" 
-    aria-valuemin="0" 
-    aria-valuemax="100">
-    25%
-    </div>
+    id="twitter_update_progressbar"
+    aria-valuemin="0"
+    aria-valuemax="100"></div>
   `;
   document.getElementById("twitter_progress").appendChild(progress);
+
+  let i = document.getElementById("twitter_select").value;
+  var display = document.getElementById('twitter_progress');
+  startTimer(i, display);
 })
 
 function generate_prediction(elmnt, clr) {
